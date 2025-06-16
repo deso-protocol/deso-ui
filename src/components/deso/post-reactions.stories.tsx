31cad228a5f6cffcb943489cafb2e5aa49d8404f
mossified <ed@mossified.com>
2025-06-16 15:59:55 -0600

    fix build error pt 2

--- src/components/deso/post-reactions.stories.tsx
+++ src/components/deso/post-reactions.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { PostReactions, Reaction } from './post-reactions';
+import PostReactions, { Reaction } from './post-reactions';
 import { useState } from 'react';
 
 const meta: Meta<typeof PostReactions> = {
